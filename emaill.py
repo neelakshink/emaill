f34ecd70c8b66d23d47185df354bd02f2eb66427
neelakshink <kaundalneelakshi@gmail.com>
2025-07-08 15:29:27 +0530

    Create emaill.py

---
+++ emaill.py
@@ -0,0 +1,26 @@
+import os
+import smtplib
+
+
+os.environ['EMAIL_USER'] = 'neelakshikaundal89@gmail.com'
+os.environ['EMAIL_PASS'] = 'blbr exiw koui mhwp'
+
+
+sender_email = os.environ.get('EMAIL_USER')
+password = os.environ.get('EMAIL_PASS')
+
+receiver_email = 'manishbissau04@gmail.com'
+subject = 'Simple Email'
+body = 'Hello, this is a simple email sent using Python and os module.'
+
+message = f"Subject: {subject}\n\n{body}"
+
+try:
+    with smtplib.SMTP('smtp.gmail.com', 587) as server:
+        server.starttls()  # Secure the connection
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message)
+        print("Email sent successfully!")
+
+except Exception as e:
+    print("Error:", e)
